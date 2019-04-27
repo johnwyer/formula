365358bfcb8b7b26ae52b23b9eabf60bcde4be1e
alexander_7 <alexx.md@gmail.com>
2019-04-27 19:24:32 +0300

    track details working

--- client/src/components/Calendar/detail.js
+++ client/src/components/Calendar/detail.js
@@ -17,7 +17,7 @@ class CalendarDetailIndex extends Component {
                         <picture className="hero-image">
                             <img src="https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/year%20icon/2019.png.transform/4col/image.png" alt="2019" />
                         </picture>
-                        <p className="race-weekend-dates">14-17 mar</p>
+                        <p className="race-weekend-dates">14 - 17 mar</p>
                     </div>
                 </div>                
                 <div className="race-review-wrapper">
@@ -56,11 +56,62 @@ class CalendarDetailIndex extends Component {
                                 </div>
                             </div>
                         </fieldset>
-                        <div className="race-map">
-                            <fieldset>
+                        <div className="race-map-wrapper">
+                            <fieldset className="race-map">
                                 <legend>
-
+                                    <span className="f1-flag-wrapper">
+                                        <picture>
+                                            <img src="https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Flags%2016x9/australia-flag.png.transform/9col/image.png" alt="australia-flag.png" />
+                                        </picture>
+                                    </span>
+                                    <span className="f1-track-name">Melbourne Grand Prix Circuit</span>
                                 </legend>
+                                <div className="row">
+                                    <div className="col-xl-7 f1-race-map-wrapper">
+                                        <p class="f1-race-map-track-name">Melbourne Grand Prix Circuit</p>
+                                        <picture className="f1-race-map-image">
+                                            <img src="https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Australia_Circuit.png.transform/9col/image.png" alt="Australia_Circuit.png" />
+                                        </picture>
+                                    </div>
+                                    <div className="col-xl-5">
+                                        <div className="row">
+                                            <div className="col-xl-12">
+                                                <div className="row">
+                                                    <div className="col-md-6">
+                                                        <div className="f1-stat">
+                                                            <p className="f1-stat-label">First Grand Prix</p>
+                                                            <p className="f1-stat-value">1996</p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="f1-stat">
+                                                            <p className="f1-stat-label">Number of Laps</p>
+                                                            <p className="f1-stat-value">58</p>
+                                                        </div>
+                                                    </div>  
+                                                    <div className="col-md-6">
+                                                        <div className="f1-stat">
+                                                            <p className="f1-stat-label">Circuit Length</p>
+                                                            <p className="f1-stat-value">5.303<span>km</span></p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <div className="f1-stat">
+                                                            <p className="f1-stat-label">Race Distance</p>
+                                                            <p className="f1-stat-value">307.574 <span>km</span></p>
+                                                        </div>
+                                                    </div>  
+                                                    <div className="col-md-12">
+                                                        <div className="f1-stat no-margin">
+                                                            <p className="f1-stat-label">Lap Record</p>
+                                                            <p className="f1-stat-value">1:24.125 <span>Michael Schumacher (2004)</span></p>
+                                                        </div>
+                                                    </div>                                                                                                                                                                                                             
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </fieldset>
                         </div>
                     </div>
