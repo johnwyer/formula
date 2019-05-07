f4449b63396d4299b5ee9e70f41247750606029f
alexander_7 <alexx.md@gmail.com>
2019-05-07 16:16:18 +0300

    calendar details site and server mods

--- client/src/components/Calendar/detail-index.js
+++ client/src/components/Calendar/detail-index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import SiteLayout from '../../hoc/site';
 import LoadingIndicator from '../utils/loading-indicator';
-import { getRaceBySlug, getTeamsDrivers } from '../../actions/site/calendar_actions';
-import moment from 'moment';
+import { getRaceBySlug } from '../../actions/site/calendar_actions';
 import CalendarDetail from './detail';
 
 import { connect } from 'react-redux';
@@ -19,65 +18,11 @@ class CalendarDetailIndex extends Component {
                 if (!this.props.site.race) {
                     this.props.history.push('/calendar');
                 } else {
-                    const { headingImage, dateEnd, track } = this.props.site.race;
-                    this.props.site.race.headingImage = (headingImage.length > 0) ? headingImage[0].url : '';
-                    this.props.site.race.isExpired = moment(dateEnd) < moment() ? true : false;
-                    this.props.site.race.track.trackConfiguration = (track.trackConfiguration.length > 0) ? track.trackConfiguration[0].url : '';
-
-                    if(this.props.site.race.isExpired && this.props.site.race.result.length !== 0) {
-                        this.props.dispatch(getTeamsDrivers()).then(() => {
-                            Array.from({ length: this.props.site.teamsDrivers.length * 2 }).forEach((item, i) => {
-                                let key = `position_${i + 1}`;
-                                let driverId = this.props.site.race.result[key].driver;
-                                let driver = {};
-
-                                this.props.site.teamsDrivers.forEach((element) => {                                     
-                                    if(element.driver_1.id === driverId) {
-                                        driver = {
-                                            id: element.driver_1.id,
-                                            firstName: element.driver_1.firstName,
-                                            lastName: element.driver_1.lastName,
-                                            teamOfficialName: element.officialName,
-                                            teamShortName: element.shortName,
-                                            teamColor: element.teamColor
-                                        };                                        
-                                    }
-                                    if(element.driver_2.id === driverId) {
-                                        driver = {
-                                            id: element.driver_2.id,
-                                            firstName: element.driver_2.firstName,
-                                            lastName: element.driver_2.lastName,
-                                            teamOfficialName: element.officialName,
-                                            teamShortName: element.shortName,
-                                            teamColor: element.teamColor
-                                        };                                        
-                                    }                                 
-
-                                    this.props.site.race.result[key].driver = driver;
-                                });
-                            });
-
-                            let results = [];
-                            for(let key in this.props.site.race.result) {
-                                if(/position_/i.test(key)) {
-                                    results.push(this.props.site.race.result[key]);
-                                }                                
-                            }
-                            this.props.site.race.result = results;
-
-                            setTimeout(() => {
-                                this.setState({
-                                    loading: false
-                                });
-                            }, 1000);
+                    setTimeout(() => {
+                        this.setState({
+                            loading: false
                         });
-                    } else {
-                        setTimeout(() => {
-                            this.setState({
-                                loading: false
-                            });
-                        }, 1000);
-                    }
+                    }, 1000);
                 }
             });
         } else {
@@ -100,7 +45,7 @@ class CalendarDetailIndex extends Component {
                 }  
             </SiteLayout>
         )
-    }
+    };
 };
 
 const mapStateToProps = (state) => {
--- server/routes/site/calendar.js
+++ server/routes/site/calendar.js
@@ -118,13 +118,64 @@ router.get('/race', (req, res) => {
             { $limit: 1 }
         ])
         .exec()
-        .then((race) => {
-            let raceNew = (race.length !== 0) ? race[0] : race;
-            if (raceNew.result.length !== 0) {
-                raceNew.result = raceNew.result[0];
+        .then(async(race2) => {
+            let race = (race2.length !== 0) ? race2[0] : race2;
+            if (race.result.length !== 0) {
+                race.result = race.result[0];
             }
 
-            return res.status(200).send(raceNew);
+            race.headingImage = (race.headingImage.length > 0) ? race.headingImage[0].url : '';
+            race.isExpired = moment(race.dateEnd) < moment() ? true : false;
+            race.track.trackConfiguration = (race.track.trackConfiguration.length > 0) ? race.track.trackConfiguration[0].url : '';
+
+            if (race.isExpired && race.result.length !== 0) {
+                let teamsDrivers = await Team.find({})
+                    .select(['id', 'teamColor', 'officialName', 'shortName', 'driver_1', 'driver_2'])
+                    .populate({ path: 'driver_1', select: 'id firstName lastName' })
+                    .populate({ path: 'driver_2', select: 'id firstName lastName' })
+                    .exec();
+
+                Array.from({ length: teamsDrivers.length * 2 }).forEach((item, i) => {
+                    let key = `position_${i + 1}`;
+                    let driverId = race.result[key].driver;
+                    let driver = {};
+
+                    teamsDrivers.forEach((element) => {
+                        if (element.driver_1.id == driverId) {
+                            driver = {
+                                id: element.driver_1.id,
+                                firstName: element.driver_1.firstName,
+                                lastName: element.driver_1.lastName,
+                                teamOfficialName: element.officialName,
+                                teamShortName: element.shortName,
+                                teamColor: element.teamColor
+                            };
+                        }
+                        if (element.driver_2.id == driverId) {
+                            driver = {
+                                id: element.driver_2.id,
+                                firstName: element.driver_2.firstName,
+                                lastName: element.driver_2.lastName,
+                                teamOfficialName: element.officialName,
+                                teamShortName: element.shortName,
+                                teamColor: element.teamColor
+                            };
+                        }
+
+                        race.result[key].driver = driver;
+                    });
+                });
+
+                let results = [];
+                for (let key in race.result) {
+                    if (/position_/i.test(key)) {
+                        results.push(race.result[key]);
+                    }
+                }
+                race.result = results;
+            }
+
+            return res.status(200).send(race);
         })
         .catch((error) => {
             return res.status(400).send(error);
