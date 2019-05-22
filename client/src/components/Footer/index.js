86af5bea480a06193a72af4a9efe7c9dcde4446e
alexander_7 <alexx.md@gmail.com>
2019-05-22 16:18:53 +0300

    small css mods

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-
 export default class Footer extends Component {
     render() {
         return (
             <footer className="footer" id="footer">
-                <div className="container">footer content</div>                
+                <div className="container">
+                    <div className="footer-content">
+                        <p>footer content</p>
+                    </div>                        
+                </div>                
             </footer>
         )
-    }
+    };
 };
