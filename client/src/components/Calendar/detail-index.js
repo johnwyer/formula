9d6e97509d015a16ae8f02a27f46ca6ccb8a47c6
alexander_7 <alexx.md@gmail.com>
2019-05-23 19:09:04 +0300

    calendar pages mods

--- client/src/components/Calendar/detail-index.js
+++ client/src/components/Calendar/detail-index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SiteLayout from '../../hoc/site';
 import LoadingIndicator from '../utils/loading-indicator';
+import ErrorIndicator from '../utils/error-indicator';
+
 import { getRaceBySlug } from '../../actions/site/calendar_actions';
 import CalendarDetail from './detail';
 
@@ -8,13 +10,26 @@ import { connect } from 'react-redux';
 
 class CalendarDetailIndex extends Component {
     state = {
-        loading: true
+        loading: true,
+        error: false,
+        errorMessage: ''
     };
 
     componentDidMount(){
+        this.getData();
+    };
+
+    getData = async() => {
         const raceSlug = this.props.match.params.slug;
         if(raceSlug !== undefined){
-            this.props.dispatch(getRaceBySlug(raceSlug)).then(() => {
+            this.setState({
+                loading: true,
+                error: false,
+                errorMessage: ''
+            });
+
+            try {
+                await this.props.dispatch(getRaceBySlug(raceSlug));
                 if (!this.props.site.race) {
                     this.props.history.push('/calendar');
                 } else {
@@ -23,26 +38,31 @@ class CalendarDetailIndex extends Component {
                             loading: false
                         });
                     }, 1000);
-                }
-            });
+                }                
+            } catch(error) {
+                this.setState({
+                    loading: false,
+                    error: true,
+                    errorMessage: error.toString()
+                });
+            }
         } else {
             this.props.history.push('/calendar');
         }
     };
 
     render() {
+        const { loading, error, errorMessage } = this.state;
+        const hasData = !(loading || error);
+        const errorIndicator = error ? <ErrorIndicator message={errorMessage} reloadHandler={() => this.getData()} /> : null;
+        const spinner = loading ? <LoadingIndicator /> : null;
+        const content = hasData ? <CalendarDetail race={this.props.site.race} /> : null;
+
         return (
             <SiteLayout classes="calendar-detail-page">
-                {
-                    this.state.loading ? 
-                    (
-                        <LoadingIndicator />
-                    )
-                    :
-                    ( 
-                        <CalendarDetail race={this.props.site.race} />
-                    )                    
-                }  
+                { errorIndicator }
+                { spinner }
+                { content }  
             </SiteLayout>
         )
     };
--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import SiteLayout from '../../hoc/site';
 
 import LoadingIndicator from '../utils/loading-indicator';
+import ErrorIndicator from '../utils/error-indicator';
+
 import { getRaces } from '../../actions/site/calendar_actions';
 import CalendarList from './calendar-list';
 
@@ -9,33 +11,52 @@ import { connect } from 'react-redux';
 
 class CalendarIndex extends Component {
     state = {
-        loading: true
+        loading: true,
+        error: false,
+        errorMessage: ''        
     };
 
     componentDidMount(){
-        this.props.dispatch(getRaces()).then(() => {            
+        this.getData();
+    };
+
+    getData = async() => {
+        this.setState({
+            loading: true,
+            error: false,
+            errorMessage: ''
+        });
+
+        try {
+            await this.props.dispatch(getRaces());
+
             setTimeout(() => {
                 this.setState({
                     loading: false
                 });
-            }, 1000);
-        });
+            }, 1000);            
+        } catch(error) {
+            this.setState({
+                loading: false,
+                error: true,
+                errorMessage: error.toString()
+            });
+        }
     };
 
     render() {
+        const { loading, error, errorMessage } = this.state;
+        const hasData = !(loading || error);
+        const errorIndicator = error ? <ErrorIndicator message={errorMessage} reloadHandler={() => this.getData()} /> : null;
+        const spinner = loading ? <LoadingIndicator /> : null;
+        const content = hasData ? <CalendarList races={this.props.site.races} /> : null;
+
         return (
             <SiteLayout classes="calendar-page">
                 <h2 className="page-title">F1 Schedule - 2019 FIA Formula One World Championship&reg; Race Calendar</h2>
-                {
-                    this.state.loading ? 
-                    (
-                        <LoadingIndicator />
-                    )
-                    :
-                    ( 
-                        <CalendarList races={this.props.site.races} />
-                    )                    
-                }                     
+                { errorIndicator }
+                { spinner }
+                { content }                   
             </SiteLayout>
         )
     }
